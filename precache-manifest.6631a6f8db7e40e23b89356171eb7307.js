0c93f1d8e0f5bae828e2744e2a608585ce6362e7
HSGill <harjotgill73@gmail.com>
2020-08-24 20:13:33 +1200

    Updates

---
+++ precache-manifest.6631a6f8db7e40e23b89356171eb7307.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "983314e253f1173df553",
+    "url": "/robofriends/static/css/main.dede0ece.chunk.css"
+  },
+  {
+    "revision": "983314e253f1173df553",
+    "url": "/robofriends/static/js/main.983314e2.chunk.js"
+  },
+  {
+    "revision": "fa0fe3f4984f0d53d042",
+    "url": "/robofriends/static/css/1.6af6a7f6.chunk.css"
+  },
+  {
+    "revision": "fa0fe3f4984f0d53d042",
+    "url": "/robofriends/static/js/1.fa0fe3f4.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "4c594d2547e86264a370ae40d9e3a109",
+    "url": "/robofriends/index.html"
+  }
+];
